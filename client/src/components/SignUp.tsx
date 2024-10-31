8a9e5d4cbdfa0f01ba59c8cb357f0c767ef116c3
seafood <mail.seafood@proton.me>
2024-10-31 14:01:23 +0300

    added login and register functionality to client

--- client/src/components/SignUp.tsx
+++ client/src/components/SignUp.tsx
@@ -9,20 +9,21 @@ interface SignUpProps {
 interface User {
   username: string;
   password: string;
+  email: string;
 }
 
 const SignUp: React.FC<SignUpProps> = ({ setIsAuth }) => {
   const cookies = new Cookies();
-  const [user, setUser] = useState<User>({ username: "", password: "" });
+  const [user, setUser] = useState<User>({ email: "", username: "", password: "" });
 
   const signUp = () => {
     Axios.post(`${import.meta.env.VITE_API_URL}/auth/register`, user).then((res) => {
-      const { token, userId, username, hashedPassword } = res.data;
+      const { token } = res.data;
 
       cookies.set("token", token);
-      cookies.set("userId", userId);
-      cookies.set("username", username);
-      cookies.set("hashedPassword", hashedPassword);
+      // cookies.set("userId", userId);
+      // cookies.set("username", username);
+      // cookies.set("password", password);
       setIsAuth(true);
     });
   };
@@ -35,6 +36,12 @@ const SignUp: React.FC<SignUpProps> = ({ setIsAuth }) => {
   return (
     <div className="flex flex-col gap-4">
       <label>Sign Up</label>
+      <input
+        name="email"
+        placeholder="Email"
+        onChange={handleInputChange}
+        required
+      />
       <input
         name="username"
         placeholder="Username"
--- server/src/middlewares/authMiddleware.js
+++ server/src/middlewares/authMiddleware.js
@@ -1,14 +1,19 @@
 import jwt from "jsonwebtoken";
-import users from "../models/userModel.js";
+import prisma from "../../prisma/prismaClient.js"
 
 export const authenticateToken = (req, res, next) => {
   const token = req.headers["authorization"];
   if (!token) return res.sendStatus(401);
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, user) => {
     if (err) return res.sendStatus(403);
-    if (!users[user.username]) return res.sendStatus(404);
-    req.user = user;
+
+    const dbUser = await prisma.user.findUnique({
+      where: { username: user.username },
+    });
+
+    if (!dbUser) return res.sendStatus(404);
+    req.user = dbUser;
     next();
   });
 };
