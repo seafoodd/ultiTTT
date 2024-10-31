1f709918e94d26c779804b6e4c0b6e6bc635f45d
seafood <mail.seafood@proton.me>
2024-10-31 13:41:27 +0300

    added register and login functions

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,34 +1,97 @@
-import { v4 as uuidv4 } from "uuid";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import users from "../models/userModel.js";
+import prisma from "../../prisma/prismaClient.js";
 
-export const signup = async (req, res) => {
+export const register = async (req, res) => {
+  const { email, username, password } = req.body;
+
+  if (!email || !username || !password) {
+    return res.status(400).json({ error: "All fields are required." });
+  }
+
   try {
-    const { username, password } = req.body;
-    const userId = uuidv4();
+    const existingEmail = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingEmail) {
+      return res.status(400).json({ error: "Email is already taken." });
+    }
+    // TODO: email verification
+
+    const existingUsername = await prisma.user.findUnique({
+      where: { username },
+    });
+
+    if (existingUsername) {
+      return res.status(400).json({ error: "Username is already taken." });
+    }
+
+    const displayName = username;
+
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    users[username] = { userId, username, hashedPassword };
-    const token = jwt.sign(
-      { username, userId },
-      process.env.ACCESS_TOKEN_SECRET,
-    );
-    console.log(users)
-    res.status(201).json({ token, userId, username, hashedPassword });
+    const user = await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        username,
+        displayName,
+      },
+    });
+
+    res.status(201).json(user);
   } catch (e) {
     console.log(e);
-    res.status(500).json("Something went wrong.");
+    res.status(500).json({ error: "Something went wrong." });
   }
 };
 
 export const login = async (req, res) => {
+  const { username, password, rememberMe } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ error: "All fields are required." });
+  }
+
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        OR: [{ email: username }, { username: username }],
+      },
+    });
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ error: "Sorry, we could not find your account." });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: "Wrong password!" });
+    }
+
+    const tokenOptions = rememberMe ? {} : { expiresIn: "1h" };
+    const token = jwt.sign(
+      { username, userId: user.userId },
+      process.env.ACCESS_TOKEN_SECRET,
+      tokenOptions,
+    );
+
+    res.status(200).json({ token });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ error: "Something went wrong." });
+  }
+
   try {
     const { username, password, rememberMe } = req.body;
     const user = users[username];
     if (!user) return res.status(404).json({ message: "User not found." });
 
-    const passwordMatch = await bcrypt.compare(password, user.hashedPassword);
+    const passwordMatch = await bcrypt.compare(password, user.password);
     if (passwordMatch) {
       const tokenOptions = rememberMe ? {} : { expiresIn: "1h" };
       const token = jwt.sign(
--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { signup, login, verifyToken } from "../controllers/authController.js";
+import { register, login, verifyToken } from "../controllers/authController.js";
 import { authenticateToken } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/signup", signup);
+router.post("/register", register);
 router.post("/login", login);
 router.get("/verifyToken", authenticateToken, verifyToken);
 
