f1a13258170f85fcce0078592479a16140ad941f
seafood <mail.seafood@proton.me>
2024-11-03 15:11:19 +0300

    added support for saving games to the players' gameHistory

--- server/src/controllers/gameController.js
+++ server/src/controllers/gameController.js
@@ -1,6 +1,7 @@
 import { checkWin, checkTie, checkOverallWin } from "../utils/gameUtils.js";
-import users from "../models/userModel.js";
-import games from "../models/gameModel.js"
+// import users from "../models/userModel.js";
+import prisma from "../../prisma/prismaClient.js";
+import games from "../models/gameModel.js";
 
 export const handleMove = (game, subBoardIndex, squareIndex, player) => {
   game.board[subBoardIndex].squares[squareIndex] = player;
@@ -16,7 +17,7 @@ export const handleMove = (game, subBoardIndex, squareIndex, player) => {
   }
 };
 
-export const handleOverallWin = (io, game, gameId) => {
+export const handleOverallWin = async (io, game, gameId) => {
   const overallWinner = checkOverallWin(game.board);
   const gameResult = {
     board: game.board,
@@ -25,32 +26,88 @@ export const handleOverallWin = (io, game, gameId) => {
     moveHistory: game.moveHistory,
   };
 
-  if (overallWinner || game.board.every((subBoard) => subBoard.subWinner !== "")) {
+  if (
+    overallWinner ||
+    game.board.every((subBoard) => subBoard.subWinner !== "")
+  ) {
     io.to(gameId).emit("gameResult", gameResult);
 
-    for (const player of game.players) {
-      const user = users[player.username];
-      if (user) {
-        if (!user.gameHistory) {
-          user.gameHistory = [];
-        }
-        user.gameHistory.push(gameResult);
-        console.log(user);
-      } else {
-        console.error(`Player with username ${player.username} not found in users object.`);
-        console.log(gameResult)
+    // for (const player of game.players) {
+    //   const user = users[player.username];
+    //   if (user) {
+    //     if (!user.gameHistory) {
+    //       user.gameHistory = [];
+    //     }
+    //     user.gameHistory.push(gameResult);
+    //     console.log(user);
+    //   } else {
+    //     console.error(`Player with username ${player.username} not found in users object.`);
+    //     console.log(gameResult)
+    //   }
+    // }
+
+    try {
+      const player1 = await prisma.user.findUnique({
+        where: { username: game.players[0].username },
+      });
+      const player2 = await prisma.user.findUnique({
+        where: { username: game.players[1].username },
+      });
+
+      if (player1 && player2) {
+        const player1EloChange = calculateEloChange(
+          player1.elo,
+          player2.elo,
+          overallWinner === game.players[0].symbol,
+        );
+        const player2EloChange = calculateEloChange(
+          player2.elo,
+          player1.elo,
+          overallWinner === game.players[1].symbol,
+        );
+
+        await prisma.game.create({
+          data: {
+            board: game.board,
+            winner: overallWinner || "none",
+            moveHistory: game.moveHistory,
+            player1: { connect: { id: player1.id } },
+            player1Elo: player1.elo,
+            player1EloChange: player1EloChange,
+            player2: { connect: { id: player2.id } },
+            player2Elo: player2.elo,
+            player2EloChange: player2EloChange,
+          },
+        });
+
+        await prisma.user.update({
+          where: { id: player1.id },
+          data: { elo: player1.elo + player1EloChange },
+        });
+
+        await prisma.user.update({
+          where: { id: player2.id },
+          data: { elo: player2.elo + player2EloChange },
+        });
       }
+    } catch (error) {
+      console.error("Error saving game result:", error);
     }
 
-    finishGame(io, game, gameId)
+    finishGame(io, game, gameId);
   }
+};
+
+const calculateEloChange = (playerElo, opponentElo, isWinner) => {
+  // TODO: Implement an actual elo system
+  return isWinner ? 10 : -10;
 };
 
 const finishGame = (io, game, gameId) => {
   io.to(gameId).disconnectSockets(true);
   clearInterval(game.timerInterval);
   delete games[gameId];
-}
+};
 
 export const updateCurrentSubBoard = (game, squareIndex) => {
   game.currentSubBoard = squareIndex;
@@ -67,7 +124,7 @@ export const startTimer = (io, game, gameId) => {
         winner: game.turn === "X" ? "O" : "X",
         state: "Won",
       });
-      finishGame(io, game, gameId)
+      finishGame(io, game, gameId);
     }
   }, 1000);
 };
