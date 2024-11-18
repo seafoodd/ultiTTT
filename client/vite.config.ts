6a4cbe5f4e4155fc88411b79c6ba1cf5639a1336
seafood <mail.seafood@proton.me>
2024-11-18 18:07:25 +0300

    added docker support

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,9 +6,15 @@ import postcssConfig from "./postcss.config";
 config();
 
 export default defineConfig({
+  base: '/',
   server: {
-    host: "192.168.0.100",
-    port: 8000,
+    // host: "192.168.0.100",
+    // port: 8000,
+    open: true,
+  },
+  build: {
+    outDir: "build",
+    sourcemap: true,
   },
   plugins: [react()],
   css: {
