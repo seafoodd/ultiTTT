79380b765bdb12f63a41155ade66a8d226c0fcc9
seafood <mail.seafood@proton.me>
2024-11-23 23:22:19 +0300

    Add disabled prop

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -5,14 +5,16 @@ interface ButtonProps {
   className?: string;
   text?: string;
   icon?: any;
+  disabled?: boolean
 }
 
-const Button: React.FC<ButtonProps> = ({ onClick, className, text, icon }) => {
+const Button: React.FC<ButtonProps> = ({ onClick, className, text, icon, disabled }) => {
   return (
     <button
       className={`${className ? className : ""} font-semibold flex justify-center 
       items-center gap-1 rounded-md h-fit text-xl shadow-md transition-colors text-nowrap`}
       onClick={onClick}
+      disabled={disabled}
     >
       {icon && icon}
       {text && text}
