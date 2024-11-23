ada122ec2f983d9e90ab5ea27dccfcf6f1f747db
seafood <mail.seafood@proton.me>
2024-11-24 01:44:01 +0300

    Add dynamic title and icon

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,61 @@
 import "./App.css";
-import { Outlet } from "react-router-dom";
+import {Outlet, useLocation} from "react-router-dom";
 import Header from "./components/Header";
 import Notification from "./components/Notification";
 import {Socket} from "socket.io-client";
-import React from "react";
+import React, {useEffect} from "react";
 
 interface AppProps {
   socket: Socket;
 }
 
 const App: React.FC<AppProps> = ({socket}) => {
+  const location = useLocation();
+
+  useEffect(() => {
+    const path = location.pathname;
+    let title = "ultiTTT"; // Default title
+
+    switch (path) {
+      case "/home":
+        title = "Home - ultiTTT";
+        break;
+      case "/about":
+        title = "About - ultiTTT";
+        break;
+      case "/blog":
+        title = "Blog - ultiTTT";
+        break;
+      case "/rules":
+        title = "Rules - ultiTTT";
+        break;
+      case "/donate":
+        title = "Donate - ultiTTT";
+        break;
+      case "/friends":
+        title = "Friends - ultiTTT";
+        break;
+      case "/login":
+        title = "Log In - ultiTTT";
+        break;
+      case "/signup":
+        title = "Sign Up - ultiTTT";
+        break;
+      case "/settings":
+        title = "Settings - ultiTTT";
+        break;
+      default:
+        if (path.startsWith("/@/")) {
+          title = "Profile - ultiTTT";
+        } else if (path.startsWith("/")) {
+          title = "Game - ultiTTT";
+        }
+        break;
+    }
+
+    document.title = title;
+  }, [location]);
+
   return (
     <>
       <Header socket={socket} />
