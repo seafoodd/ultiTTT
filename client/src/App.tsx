f82cfe52a2ac03c998ba221c8fe92d8ab2f21484
seafood <mail.seafood@proton.me>
2024-12-26 16:18:51 +0300

    Reduce margin top

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -55,7 +55,7 @@ const App = () => {
   return (
     <>
       <Header />
-      <div className="w-full h-full mt-28 flex justify-center items-center gap-24">
+      <div className="w-full h-full mt-24 flex justify-center items-center gap-24">
         <Outlet />
         <Notification />
       </div>
