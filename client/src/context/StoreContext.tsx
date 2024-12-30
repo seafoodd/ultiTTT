8e6ff9964713be26c773953c882c747480675961
seafood <mail.seafood@proton.me>
2024-12-30 16:55:17 +0300

    Add StoreContext, add/remove friend support

--- client/src/context/StoreContext.tsx
+++ client/src/context/StoreContext.tsx
@@ -6,14 +6,18 @@ import React, {
   useEffect,
 } from "react";
 import { useAuth } from "./AuthContext";
+import Axios from "axios";
 
 interface StoreContextProps {
   friends: string[];
+  setFriends: React.Dispatch<React.SetStateAction<string[]>>;
   fetchFriends: Function;
+  sendFriendRequest: Function;
   activeGames: any[];
   setActiveGames: React.Dispatch<React.SetStateAction<any[]>>;
   incomingRequests: any[];
   outgoingRequests: any[];
+  loading: boolean;
 }
 
 const StoreContext = createContext<StoreContextProps | undefined>(undefined);
@@ -21,57 +25,92 @@ const StoreContext = createContext<StoreContextProps | undefined>(undefined);
 export const StoreProvider = ({ children }: { children: ReactNode }) => {
   const { token, isAuth } = useAuth();
   const [friends, setFriends] = useState<string[]>([]);
-  const [incomingRequests, setIncomingRequests] = useState<any[]>([]);
-  const [outgoingRequests, setOutgoingRequests] = useState<any[]>([]);
+  const [incomingRequests, setIncomingRequests] = useState<
+    { username: string; id: string }[]
+  >([]);
+  const [outgoingRequests, setOutgoingRequests] = useState<
+    { username: string; id: string }[]
+  >([]);
   const [activeGames, setActiveGames] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     if (!isAuth) return;
-    fetchFriends(
-      token!,
-      setFriends,
-      setIncomingRequests,
-      setOutgoingRequests,
-    ).catch((err) => {
-      console.log(err);
+    fetchFriends().catch((err) => {
+      console.error(err);
     });
   }, [token, isAuth]);
 
+  const fetchFriends = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/friends`, {
+        headers: { Authorization: token! },
+      });
+      if (!response.ok) {
+        return console.error("Error fetching friends");
+      }
+      const data = await response.json();
+      setFriends(data.friends);
+      setIncomingRequests(
+        data.incomingRequests.map(
+          (r: { id: string; toUsername: string; fromUsername: string }) => ({
+            id: r.id,
+            username: r.fromUsername,
+          }),
+        ),
+      );
+      setOutgoingRequests(
+        data.outgoingRequests.map(
+          (r: { id: string; toUsername: string; fromUsername: string }) => ({
+            id: r.id,
+            username: r.toUsername,
+          }),
+        ),
+      );
+    } catch (error) {
+      console.error("Error fetching friends:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const sendFriendRequest = (username: string, action: "add" | "remove") => {
+    Axios.post(
+      `${import.meta.env.VITE_API_URL}/friends/${action}/${username}`,
+      {},
+      {
+        headers: {
+          Authorization: token!,
+        },
+      },
+    )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((e) => {
+        console.error(e.message);
+      })
+      .finally(fetchFriends);
+  };
+
   return (
     <StoreContext.Provider
-      value={{ friends, fetchFriends, activeGames, setActiveGames, incomingRequests, outgoingRequests }}
+      value={{
+        friends,
+        setFriends,
+        fetchFriends,
+        sendFriendRequest,
+        activeGames,
+        setActiveGames,
+        incomingRequests,
+        outgoingRequests,
+        loading,
+      }}
     >
       {children}
     </StoreContext.Provider>
   );
-};
-
-const fetchFriends = async (
-  token: string,
-  setFriends: Function,
-  setIncomingRequests: Function,
-  setOutgoingRequests: Function,
-) => {
-  const response = await fetch(`${import.meta.env.VITE_API_URL}/friends`, {
-    headers: { Authorization: token },
-  });
-  if (!response.ok) {
-    if (response.status === 401) {
-      console.log("Unauthorized");
-    } else {
-      console.log("Something went wrong.");
-    }
-  } else {
-    const data = await response.json();
-
-    const { friends, incomingRequests, outgoingRequests } = data;
-
-    // console.log(data, friends, incomingRequests, outgoingRequests);
-
-    setFriends(friends);
-    setIncomingRequests(incomingRequests);
-    setOutgoingRequests(outgoingRequests);
-  }
 };
 
 export const useStore = () => {
