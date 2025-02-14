9d6742334f1783b8c7b6177b7be732bc18868a01
seafood <mail.seafood@proton.me>
2025-02-14 21:01:11 +0300

    update modal design

--- client/src/components/ChallengeModal.tsx
+++ client/src/components/ChallengeModal.tsx
@@ -32,31 +32,37 @@ const ChallengeModal: React.FC<ChallengeModalProps> = ({ username }) => {
   return (
     <div className="flex flex-col items-center">
       <h1 className="font-bold truncate max-w-64">Play against {username}</h1>
-      <p className="mt-20 font-semibold">Minutes per player:</p>
-      <div className="flex gap-3 justify-center items-center mt-4">
+      <p className="mt-12 font-medium font-xl">Minutes per player:</p>
+      <div className="grid grid-cols-2 gap-3 justify-center items-center mt-4">
+        <button
+          onClick={() => setGameType("2")}
+          className={`${gameType === "2" ? "bg-color-accent-400 text-white border-color-accent-400" : "hover:bg-color-accent-400/50 border-white"} text-xl font-bold w-20 h-16 border rounded-md`}
+        >
+          2 + 1
+        </button>
         <button
           onClick={() => setGameType("5")}
-          className={`${gameType === "5" ? "bg-color-blue-2" : "hover:bg-color-blue-2/50"} font-bold w-20 h-16 border-2 border-white rounded-md`}
+          className={`${gameType === "5" ? "bg-color-accent-400 text-white border-color-accent-400" : "hover:bg-color-accent-400/50 border-white"} text-xl font-bold w-20 h-16 border rounded-md`}
         >
-          5
+          5 + 3
         </button>
         <button
           onClick={() => setGameType("10")}
-          className={`${gameType === "10" ? "bg-color-blue-2" : "hover:bg-color-blue-2/50"} font-bold w-20 h-16 border-2 border-white rounded-md`}
+          className={`${gameType === "10" ? "bg-color-accent-400 text-white border-color-accent-400" : "hover:bg-color-accent-400/50 border-white"} text-xl font-bold w-20 h-16 border rounded-md`}
         >
-          10
+          10 + 5
         </button>
         <button
           onClick={() => setGameType("15")}
-          className={`${gameType === "15" ? "bg-color-blue-2" : "hover:bg-color-blue-2/50"} font-bold w-20 h-16 border-2 border-white rounded-md`}
+          className={`${gameType === "15" ? "bg-color-accent-400 text-white border-color-accent-400" : "hover:bg-color-accent-400/50 border-white"} text-xl font-bold w-20 h-16 border rounded-md`}
         >
-          15
+          15 + 10
         </button>
       </div>
       <Button
         onClick={sendChallengeRequest}
         text="Challenge"
-        className="bg-color-blue-2 mt-auto mb-4 py-2 w-32 hover:bg-color-blue-2/75"
+        className="bg-color-accent-400 mt-auto mb-4 py-2 w-32 hover:bg-color-accent-500"
       />
     </div>
   );
