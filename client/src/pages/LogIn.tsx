8d17179524bb33fb7e1937a38732117c4332a36a
seafood <mail.seafood@proton.me>
2025-02-14 19:26:05 +0300

    update log in page design

--- client/src/pages/LogIn.tsx
+++ client/src/pages/LogIn.tsx
@@ -61,28 +61,28 @@ const LogIn = () => {
 
   return (
     <div
-      className="flex flex-col bg-color-gray-1 p-8 rounded-md
-      justify-center mt-24"
+      className="relative flex flex-col bg-color-neutral-850 p-8 rounded-md
+      justify-center mt-24 shadow-color-neutral-800 drop-shadow-lg"
     >
-      <h1 className="flex justify-center text-white/90 text-3xl font-medium">
-        Log in
+      <h1 className="absolute top-0 left-0 w-full text-color-accent-100 text-2xl font-medium py-2 border-b-2 border-color-accent-300">
+        Log In
       </h1>
-      <form className="mt-12 flex flex-col gap-4 w-72" onSubmit={logIn}>
+      <form className="mt-16 flex flex-col gap-4 w-72" onSubmit={logIn}>
         <input
-          className="px-2 py-1 rounded-md"
+          className="px-2 py-1.5 rounded-md bg-color-neutral-900 placeholder-color-accent-100/40"
           placeholder="Username or email"
           onChange={(event) => setIdentifier(event.target.value)}
           required
         />
         <input
-          className="px-2 py-1 rounded-md"
+          className="px-2 py-1.5 rounded-md bg-color-neutral-900 placeholder-color-accent-100/40"
           placeholder="Password"
           type="password"
           onChange={(event) => setPassword(event.target.value)}
           required
         />
         <div className="flex justify-between">
-          <label className="text-[14px] cursor-pointer font-medium text-white/90 gap-1 flex items-center justify-center">
+          <label className="text-[14px] cursor-pointer font-medium opacity-90 gap-1 flex items-center justify-center">
             <input
               className="cursor-pointer"
               type="checkbox"
@@ -92,26 +92,27 @@ const LogIn = () => {
             Remember Me
           </label>
           <button
-            className="text-[14px] underline font-medium text-white/80 transition-colors hover:text-white"
-            onClick={logIn}
+            title="In development"
+            className="text-[14px] underline font-medium opacity-80 transition-colors hover:text-white"
+            type="button"
           >
             Forgot password?
           </button>
         </div>
         {error && <div className="text-red-500 text-md -mb-5">{error}</div>}
         <button
-          className="bg-blue-600 disabled:bg-opacity-70 mt-8 font-semibold flex justify-center items-center rounded-md px-12 w-full py-2"
+          className="bg-color-accent-400 disabled:bg-opacity-70 mt-4 font-semibold flex justify-center items-center rounded-md px-12 w-full py-2"
           disabled={loading || rateLimitTimeLeft !== null}
           type="submit"
         >
           {getButtonContent()}
         </button>
         <button
-          className="text-[14px] font-medium text-white/70 transition-colors hover:text-white/80"
+          className="text-[14px] font-medium opacity-80 transition-colors hover:opacity-90"
           onClick={() => navigate("/signup")}
         >
           Don't have an account?{" "}
-          <span className="text-white/80 font-semibold">Sign Up</span>
+          <span className="opacity-90 font-semibold">Sign Up</span>
         </button>
       </form>
     </div>
