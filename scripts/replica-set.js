dec813f82ae5ad8d51b69d9c00715b8924b6bb1c
seafood <mail.seafood@proton.me>
2025-07-01 19:03:14 +0200

    update docker-compose.yml

---
+++ scripts/replica-set.js
@@ -0,0 +1,8 @@
+try {
+    rs.status()
+} catch (err) {
+    rs.initiate({
+        _id: 'rs0',
+        members: [{ _id: 0, host: 'db:27017' }]
+    })
+}
--- server/mongo-init.js
+++
@@ -1,7 +0,0 @@
-db.createUser({
-  user: process.env.MONGO_INITDB_USERNAME,
-  pwd: process.env.MONGO_INITDB_PASSWORD,
-  roles: [{ role: "root", db: "admin" }]
-});
-
-rs.initiate();
